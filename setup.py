c8c803359833b14332db0409e13e9a75e9cdb343
Humairabh <hbh4bv@virginia.edu>
2022-07-15 23:07:34 -0400

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='package',
+      version='0.1',
+      description='Final exam package',
+      url='https://github.com/Humairabh/DS5100_FinalProject',
+      author='Humaira Halim',
+      author_email='hbh4bv@virginia.com',
+      license='MIT',
+      packages=['package'])
